b2f460dd8833bfd32606796ea30c7b9c4bbe93cd
Hector Jair Velazquez Jimenez <hector_velazquez@hacienda.gob.mx>
2026-09-01 11:26:01 -0600

    cambio de ruta de produccion

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig(({ command }) => {
       },
     },
 
-    base: isDev ? '/' : '/work/models/PTP/NPTP/PTP_Doc_Ciudadanos/SRFT/',
+    base: isDev ? '/' : '/work/models/PTP/NPTP/PTP_SRFT/',
 
     build: {
       emptyOutDir: true,
